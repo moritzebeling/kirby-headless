c0dfcaba9bcd7f7a1c5475f3359d6c647ddee734
moritzebeling <git@moritzebeling.com>
2021-01-29 22:01:45 +0100

    outsource jsonResponse class

--- index.php
+++ index.php
@@ -1,55 +1,8 @@
 <?php
 
-class jsonResponse {
+load([ 'moritzebeling\\headless\\jsonResponse' => 'src/jsonResponse.php' ], __DIR__);
 
-	protected $request;
-	protected $data;
-	protected $status;
-	protected $cached;
-
-	public function __construct( string $request = '', ?array $data = null, int $status = 200, bool $cached = false )
-	{
-		$this->request = $request;
-		$this->data = $data;
-		$this->status = $status;
-		$this->cached = $cached;
-	}
-
-	public function data( ?array $data = null, ?bool $cached = null ){
-		if( $data !== null ){
-			$this->data = $data;
-		}
-		if( $cached !== null ){
-			$this->cached = $cached;
-		}
-		return $this->data;
-	}
-
-	public function status( ?int $status = null ){
-		if( $status !== null ){
-			$this->status = $status;
-		}
-		return $this->status;
-	}
-
-	public function cached( ?bool $cached = null ){
-		if( $cached !== null ){
-			$this->cached = $cached;
-		}
-		return $this->cached;
-	}
-
-	public function json(): array
-	{
-		return [
-			'status' => $this->status,
-			'request' => $this->request,
-			'cached' => $this->cached,
-			'data' => $this->data,
-		];
-	}
-
-}
+use moritzebeling\headless\jsonResponse as jsonResponse;
 
 Kirby::plugin('moritzebeling/headless', [
 
